865b1498a95fc97b00d93472cd75865795638eba
Ren <renrror@gmail.com>
2023-03-16 19:02:00 +0700

    Initial commit

---
+++ src/test/java/id/ac/ui/cs/advprog/touring/accountwallet/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.touring.accountwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
