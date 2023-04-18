be674a7bac6f9f8fed3b38db734ddbfc6102a5e4
Alfredo <alfredo.austin@ui.ac.id>
2023-04-18 23:23:06 +0700

    fix: fix email content and enabling cors globally

--- src/main/java/id/ac/ui/cs/advprog/touring/accountwallet/controller/AuthController.java
+++ src/main/java/id/ac/ui/cs/advprog/touring/accountwallet/controller/AuthController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 public class AuthController {
     private final AuthService authService;
 
