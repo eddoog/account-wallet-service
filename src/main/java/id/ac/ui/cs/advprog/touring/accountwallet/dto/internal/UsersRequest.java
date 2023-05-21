db741b58e8ca9161e8037800b920f2b9c821704b
Rendy Arya Kemal <renrror@gmail.com>
2023-05-21 22:47:21 +0700

    feat: add ability for internal to fetch arbitrary user

---
+++ src/main/java/id/ac/ui/cs/advprog/touring/accountwallet/dto/internal/UsersRequest.java
@@ -0,0 +1,14 @@
+package id.ac.ui.cs.advprog.touring.accountwallet.dto.internal;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UsersRequest {
+    Integer[] userIds;
+}
